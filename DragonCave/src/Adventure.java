f484ca5b41c084f1b6e2f454a9c60fc3faac9a96
DuncanClotfelter <duncan@clotfelter.net>
2022-03-21 15:49:15 -0500

    Initial commit

---
+++ DragonCave/src/Adventure.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Adventure {
+    public static void main(String[] args) {
+        System.out.println("You are in a land full of dragons. In front of you,");
+        System.out.println("you see two caves. In one cave, the dragon is friendly");
+        System.out.println("and will share his treasure with you. The other dragon");
+        System.out.println("is greedy and hungry and will eat you on sight.");
+        System.out.println("Which cave will you go into? (1 or 2)");
+
+        Scanner input = new Scanner(System.in);
+        int response = 0;
+        try { response = Integer.parseInt(input.nextLine()); }
+        catch(NumberFormatException e) {/* leave response as 0 */}
+
+        System.out.println("You approach the cave...");
+        System.out.println("It is dark and spooky...");
+        System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+
+        String result;
+        if(response == 1) {
+            result = "Gobbles you down in one bite!";
+        } else if(response == 2) {
+            result = "Bestows upon you a fortune fit for a king!";
+        } else {//Any unexpected response
+            result = "Hurts itself in its confusion!";
+        }
+
+        System.out.println(result);
+    }
+}
